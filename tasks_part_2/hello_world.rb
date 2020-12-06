0e9bd9ee976a5ccff78485051e27b3c8294ca3c1
aleksandrshliakhovoi <artidex028@gmail.com>
2020-12-06 19:40:47 +0200

    first task from part2

---
+++ tasks_part_2/hello_world.rb
@@ -0,0 +1,17 @@
+text1 = 'Hello'
+text2 = 'World'
+
+
+def word_from_chars(*arg)
+  if(arg[0] != nil)
+  initial = 0
+    while initial < arg[0].size
+      puts arg[0][initial]
+      initial += 1 
+    end  
+  end
+end
+
+
+var = word_from_chars(text1, text2)
+
